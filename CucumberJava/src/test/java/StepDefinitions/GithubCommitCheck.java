2881593703706660d1e9acc9b3975a566c44c0ea
Bittu Kumar <Bittu Kumar@PRATIKSONYLAPTOP>
2025-02-02 00:13:58 +0530

    1st commit new class is added to mit testing package for commit

--- CucumberJava/src/test/java/StepDefinitions/GithubCommitCheck.java
+++ CucumberJava/src/test/java/StepDefinitions/GithubCommitCheck.java
@@ -3,7 +3,7 @@ package StepDefinitions;
 public class GithubCommitCheck {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("xyz");
 
 	}
 
--- saleniumwebdriver/src/test/java/Day39/BrokenLinks.java
+++ saleniumwebdriver/src/test/java/Day39/BrokenLinks.java
@@ -3,8 +3,14 @@ package Day39;
 public class BrokenLinks {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
